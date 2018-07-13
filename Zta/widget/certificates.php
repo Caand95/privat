aba493729430226f5c5361fb4d34161bf77ae253
Casper Andersen <zbccaand49@zbc.dk>
2018-07-13 19:01:18 +0200

    font?

--- Zta/widget/certificates.php
+++ Zta/widget/certificates.php
@@ -1,5 +1,5 @@
 <section class="certificates<?php if(!isset($_GET["page"]) || (isset($_GET["page"]) && $_GET["page"] == "Forside")){echo"-front";} ?>">
-    <img class="oneby1<?php if(!isset($_GET["page"]) || (isset($_GET["page"]) && $_GET["page"] == "Forside")){echo"-front";} ?>" src="pics/logo65x65.png" />
-    <img class="oneby1<?php if(!isset($_GET["page"]) || (isset($_GET["page"]) && $_GET["page"] == "Forside")){echo"-front";} ?>" src="pics/logo65x65.png" />
-    <img class="oneby1<?php if(!isset($_GET["page"]) || (isset($_GET["page"]) && $_GET["page"] == "Forside")){echo"-front";} ?>" src="pics/logo65x65.png" />
+    <img class="oneby1<?php if(!isset($_GET["page"]) || (isset($_GET["page"]) && $_GET["page"] == "Forside")){echo"-front";} ?>" src="pics/certicate1.png" />
+    <img class="oneby1<?php if(!isset($_GET["page"]) || (isset($_GET["page"]) && $_GET["page"] == "Forside")){echo"-front";} ?>" src="pics/certicate2.png" />
+    <img class="oneby1<?php if(!isset($_GET["page"]) || (isset($_GET["page"]) && $_GET["page"] == "Forside")){echo"-front";} ?>" src="pics/certicate3.png" />
 </section>
