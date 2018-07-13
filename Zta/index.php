889f6f878885d811d1e7dd3677bde0d49fe61b9d
Casper Andersen <zbccaand49@zbc.dk>
2018-07-13 18:48:22 +0200

    Forside og b0dy &main fix

--- Zta/index.php
+++ Zta/index.php
@@ -10,10 +10,10 @@
 <body>
 
 	<!-- Header -->
-		<header class="header">
+		<header class="header <?php if(!isset($_GET["page"]) || (isset($_GET["page"]) && $_GET["page"] == "Forside")){echo"header-front";} ?>">
 			<section class="headerleft">
 				<a href="?page=Forside">
-					<img class="logo" src="pics/logo65x65.png" />
+					<img class="logo <?php if(!isset($_GET["page"]) || (isset($_GET["page"]) && $_GET["page"] == "Forside")){echo"logo-front";} ?>" src="pics/logo65x65.png" />
 
 					<h5 class="logo-text">Zoneterapi v/Lisbeth Andersen</h5>
 				</a>
@@ -27,20 +27,21 @@
 		</header>
 
 <!--banner?-->
-	<section class="banner">
+	<section class="banner<?php if(!isset($_GET["page"]) || (isset($_GET["page"]) && $_GET["page"] == "Forside")){echo"-front";} ?>">
 
 	</section>
+	<?php
+	if(!isset($_GET["page"]) || (isset($_GET["page"]) && $_GET["page"] == "Forside")){
+	} else{
 
-	<div id="b0dy">
-
-
-	<!--Main-->
-		<section class="main">
-			<?php
-				include "main.php";
-			?>
+echo '<div id="b0dy">
+			<!--Main-->
+			<section class="main">
+				';include "main.php";echo'
 		</section>
-</div>
+</div>';
+}
+?>
 
 	<!--Footer-->
 		<footer>
--- Zta/widget/certificates.php
+++ Zta/widget/certificates.php
@@ -1,5 +1,5 @@
-<section class="certificates">
-    <img class="oneby1" src="pics/logo65x65.png" />
-    <img class="oneby1" src="pics/logo65x65.png" />
-    <img class="oneby1" src="pics/logo65x65.png" />
+<section class="certificates<?php if(!isset($_GET["page"]) || (isset($_GET["page"]) && $_GET["page"] == "Forside")){echo"-front";} ?>">
+    <img class="oneby1<?php if(!isset($_GET["page"]) || (isset($_GET["page"]) && $_GET["page"] == "Forside")){echo"-front";} ?>" src="pics/logo65x65.png" />
+    <img class="oneby1<?php if(!isset($_GET["page"]) || (isset($_GET["page"]) && $_GET["page"] == "Forside")){echo"-front";} ?>" src="pics/logo65x65.png" />
+    <img class="oneby1<?php if(!isset($_GET["page"]) || (isset($_GET["page"]) && $_GET["page"] == "Forside")){echo"-front";} ?>" src="pics/logo65x65.png" />
 </section>
