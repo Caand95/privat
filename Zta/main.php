65065a6f41d7bd4cb3815596b52348f48a084958
Casper Andersen <zbccaand49@zbc.dk>
2018-07-15 17:37:38 +0200

    Startet på sidenav i main.php

--- Zta/main.php
+++ Zta/main.php
@@ -1,25 +1,31 @@
 <h5 class="breadcrums"> <?php
+//breadcrums
 	//Hvis der ikke er valgt en menu
 	if (empty($_GET['page']) || $_GET['page'] == "Forside") {
-		print "<h5>→ Forside </h5>";
+		print "//Forside </h5>";
 		include "site/Forside.php";
-		
-
+		$NoFront = 0;
 	//Hvis der er valgt en menu
 	} else if (!empty($_GET['page']) && empty($_GET['subpage'])) {
 		$page=$_GET['page'];
-		print "<h5>→ <a href=?page=".$page." > ".$page."</a> </h5>";
-		include "site/".$page."/index.php";
-
+		print "// <a href=?page=".$page." > ".$page."</a> </h5>";
+  	$NoFront = 1;
 	//Hvis der er valgt en menu og en undermenu
 	} else if (!empty($_GET['subpage'])) {
 		$page=$_GET['page'];
 		$subpage = $_GET['subpage'];
-		print "<h5>→ <a href='?page=".$page."'>".$page."</a>"." → <a href='?page=".$page."&subpage=".$subpage."'>".$subpage."</a> </h5>";
-		include "site/".$page."/".$subpage.".php";
-
+		print "// <a href='?page=".$page."'>".$page."</a>"." / <a href='?page=".$page."&subpage=".$subpage."'>".$subpage."</a> </h5>";
+		$NoFront = 2;
 	//Hvis der er angivet forkerte adresser
 	} else {
 		print "404";
+	}
+
+//Sidenav
+	if($NoFront != 0){
+		<div class="sidenav">
+		include "site/".$page."/index.php";
+		include "site/".$page."/".$subpage.".php";
+
 	}
 ?>
